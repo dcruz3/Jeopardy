85b7a7805573e4de092a4c14e65b380ab89461b1
Juan Ignacio Casal <ignaciocasal@gmail.com>
2017-03-10 13:07:18 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './containers/root';
+import './styles/index.css';
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root')
+);
